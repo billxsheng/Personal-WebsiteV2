3823c5ff001fc1c11dccacf7849429457de8d67a
Bill Sheng <billxsheng@gmail.com>
2021-08-18 21:25:19 -0400

    remove emojis

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -27,25 +27,25 @@ class Home extends Component {
                                 <h1>Bill Sheng</h1>
                             </Row>
                             <Row>
-                                <p>Hi. I'm a fourth-year&nbsp;<a target="_blank" styleName="text-link-mgte" href="https://uwaterloo.ca/management-sciences/management-engineering">Management Engineering</a>&nbsp;student at the University of Waterloo. 🔬</p>
+                                <p>Hi. I'm a fourth-year&nbsp;<a target="_blank" styleName="text-link-mgte" href="https://uwaterloo.ca/management-sciences/management-engineering">Management Engineering</a>&nbsp;student at the University of Waterloo.</p>
                             </Row>
                             {/* <Row>
                                 <p>This past winter, I interned at&nbsp;
                                     <a target="_blank" styleName="text-link-shopify" href="https://shopify.ca">Shopify</a>&nbsp;as a backend engineer on the&nbsp; 
-                                    <a target="_blank" styleName="text-link-gc" href="https://apps.shopify.com/google?surface_detail=places-to-sell-online-marketplaces&surface_inter_position=1&surface_intra_position=1&surface_type=category">Google Sales Channel</a>&nbsp;team. 🛒
+                                    <a target="_blank" styleName="text-link-gc" href="https://apps.shopify.com/google?surface_detail=places-to-sell-online-marketplaces&surface_inter_position=1&surface_intra_position=1&surface_type=category">Google Sales Channel</a>&nbsp;team.
                                 </p>
                             </Row> */}
                             <Row>
                                 <p>This upcoming fall, I will be working at&nbsp;
                                     <a target="_blank" styleName="text-link-fb" href="https://facebook.com">Facebook</a>&nbsp;as a software engineering intern on the&nbsp; 
-                                    <a target="_blank" styleName="text-link-whatsapp" href="https://www.whatsapp.com/?lang=en">WhatsApp Web</a>&nbsp;team. ⚙️
+                                    <a target="_blank" styleName="text-link-whatsapp" href="https://www.whatsapp.com/?lang=en">WhatsApp Web</a>&nbsp;team.
                                 </p>
                             </Row>
                             <Row>
                                 <Link to="/about"><Button styleName="text-link-learn-more">Learn More</Button></Link>
                             </Row>
                             <Row styleName="future-text">
-                                <p>Currently seeking <b>2022 Internship/New Grad opportunities.</b>&nbsp;View my&nbsp;<a target="_blank" href={Resume} styleName="text-link-resume">resume</a>. 👨‍💻</p>
+                                <p>Currently seeking <b>2022 Internship/New Grad opportunities.</b>&nbsp;View my&nbsp;<a target="_blank" href={Resume} styleName="text-link-resume">resume</a>.</p>
                             </Row>
                             <Row styleName="links-row">
                                 <a target="_blank" href={Resume}><FileEarmarkFill /></a>
